5d6e586510fd07f86d10f604a3bff02b423732ea
MyshinskyIlya <myshinskiyyy@gmail.com>
2023-12-15 00:52:17 +0400

    input and click events

---
+++ src/components/MyHello.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(1)
+
+function increment() {
+    console.log(count)
+    count.value++
+}
+</script>
+
+<template>
+    <h1>{{ count }}</h1>
+    <p>sadasd</p>
+    <button @click="increment">+</button>
+    <input v-model="count" />
+</template>
